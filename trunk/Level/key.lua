98337ed6a06955f1c608f8b50724fba7c201b4be
fjzusi@gmail.com <fjzusi@gmail.com@a3693c46-2507-c13d-099e-7005720a7fda>
2013-03-24 21:40:07 +0000

    Added Treasure

--- trunk/Level/key.lua
+++ trunk/Level/key.lua
@@ -2,13 +2,14 @@ Key = class("Key")
 
 KEY_WIDTH = 32
 KEY_HEIGHT = 13
+KEY_VERTICAL_OFFSET = 8
 
 KEY_IMAGE = love.graphics.newImage("Asset/Graphic/Item/Key.png")
 
 function Key:initialize(x, y)
 	self.boundedBox = {
 		x = x,
-		y = y,
+		y = y + KEY_VERTICAL_OFFSET,
 		width = KEY_WIDTH,
 		height = KEY_HEIGHT,
 		parent = self
