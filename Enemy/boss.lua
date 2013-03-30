a565bc435f3c105a4840b658bafce2464f7ff570
fjzusi@gmail.com <fjzusi@gmail.com@a3693c46-2507-c13d-099e-7005720a7fda>
2013-03-30 02:10:42 +0000

    Boss movement

--- Enemy/boss.lua
+++ Enemy/boss.lua
@@ -1,7 +1,7 @@
 Boss = class("Boss")
 
-BOSS_WIDTH = 192
-BOSS_HEIGHT = 192
+BOSS_WIDTH = 128
+BOSS_HEIGHT = 128
 BOSS_SPRITE_OFFSET_X = 0
 BOSS_SPRITE_OFFSET_Y = 0
 
@@ -11,16 +11,13 @@ BOSS_START_Y = 100
 BOSS_INVULN_TIMER = 3
 BOSS_MAX_HEALTH = 100
 BOSS_HEALTH_DECREMENT = 5
-
-BOSS_STATE_IDLING = 0
-BOSS_STATE_MOVING = 1
-BOSS_STATE_SPAWNING = 2
-BOSS_STATE_SHOOTING = 3
-BOSS_STATE_DYING = 4
+BOSS_MOVEMENT_SPEED = 150
+BOSS_MIN_DISTANCE_TO_TARGET = 30
 
 function Boss:initialize(enemyManager)
 	self.enemyManager = enemyManager
 	self.active = false
+	self.alive = true
 	
 	self.boundedBox = {
 		x = -1000,
@@ -30,9 +27,37 @@ function Boss:initialize(enemyManager)
 		parent = self
 	}
 	
-	self.curState = BOSS_STATE_IDLING
 	self.health = BOSS_MAX_HEALTH
 	self.invulnTimer = 0
+	
+	self.targetPoints = {}
+	self.targetPoints[1] = { -- Top
+		x = 448,
+		y = 128,
+		nextPoints = { 2, 3, 4 }
+	}
+	self.targetPoints[2] = { -- Left
+		x = 64,
+		y = 512,
+		nextPoints = { 1, 3, 5 }
+	}
+	self.targetPoints[3] = { -- Middle
+		x = 448,
+		y = 512,
+		nextPoints = { 1, 2, 4, 5 }
+	}
+	self.targetPoints[4] = { -- Right
+		x = 832,
+		y = 512,
+		nextPoints = { 1, 3, 5 }
+	}
+	self.targetPoints[5] = { -- Bottom
+		x = 448,
+		y = 832,
+		nextPoints = { 2, 3, 4 }
+	}
+	
+	self.curTarget = self.targetPoints[1]
 	
 	self.solidCollisions = {}
 end
@@ -42,40 +67,86 @@ function Boss:reset()
 	self.boundedBox.y = -1000
 	
 	bump.remove(self.boundedBox)
+	self.curTarget = self.targetPoints[1]
+	
 	self.active = false
+	self.alive = true
 end
 
 function Boss:activate()
-	self.boundedBox.x = BOSS_START_X
-	self.boundedBox.y = BOSS_START_Y
+	self.boundedBox.x = self.curTarget.x
+	self.boundedBox.y = self.curTarget.y
+	self:checkMovementTarget()
 	
 	bump.add(self.boundedBox)
 	self.active = true
 end
 
 function Boss:onCollision(dt, other, dx, dy)
-	if instanceOf(Bullet, other) then
-		if self.invulnTimer <= 0 then
-			self.health = self.health - BOSS_HEALTH_DECREMENT
-			
-			if self.health <= 0 then
-				-- Die
-			else
-				self.invulnTimer = BOSS_INVULN_TIMER
+	if self.active then
+		if instanceOf(Bullet, other) then
+			if self.invulnTimer <= 0 then
+				self.health = self.health - BOSS_HEALTH_DECREMENT
+				
+				if self.health <= 0 then
+					self.alive = false
+				else
+					self.invulnTimer = BOSS_INVULN_TIMER
+				end
 			end
+		elseif instanceOf(Player, other) then
+			other:hitByEnemy()
 		end
-	elseif instanceOf(Player, other) then
-		other:hitByEnemy()
 	end
 end
 
-function Boss:update(dt)
-	if self.invulnTimer > 0 then
-		self.invulnTimer = self.invulnTimer - dt
+function Boss:update(dt, playerX, playerY)
+	if self.active then
+		if self.invulnTimer > 0 then
+			self.invulnTimer = self.invulnTimer - dt
+		end
+		
+		self:updateMovement(dt)
+		self:checkMovementTarget()
+		
+		self:tryAttack(dt, playerX, playerY)
 	end
+end
+
+function Boss:updateMovement(dt)
+	local vx = self.curTarget.x - self.boundedBox.x
+	local vy = self.curTarget.y - self.boundedBox.y
+	
+	vx, vy = math.normalize(vx, vy)
+	
+	vx = vx * BOSS_MOVEMENT_SPEED * dt
+	vy = vy * BOSS_MOVEMENT_SPEED * dt
+	
+	self.boundedBox.x = self.boundedBox.x + vx
+	self.boundedBox.y = self.boundedBox.y + vy
+end
+
+function Boss:checkMovementTarget()
+	local distanceToTarget = math.dist(self.boundedBox.x, self.boundedBox.y, self.curTarget.x, self.curTarget.y)
+	
+	if distanceToTarget < BOSS_MIN_DISTANCE_TO_TARGET then
+		local nextTarget = math.random(#self.curTarget.nextPoints)
+		
+		self.curTarget = self.targetPoints[self.curTarget.nextPoints[nextTarget]]
+	end
+end
+
+function Boss:tryAttack(dt, playerX, playerY)
+	
 end
 
 function Boss:draw()
-	love.graphics.setColor(255, 255, 255)
-	love.graphics.rectangle("fill", self.boundedBox.x, self.boundedBox.y, self.boundedBox.width, self.boundedBox.height)
+	if self.active then
+		love.graphics.setColor(255, 255, 255)
+		love.graphics.rectangle("fill", self.boundedBox.x, self.boundedBox.y, self.boundedBox.width, self.boundedBox.height)
+		
+		
+		
+		
+	end
 end
